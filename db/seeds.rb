1a94115bb1d77bcf9e850d6ce0995b9a16ee5a50
Michael Flynn <mflynn37211@yahoo.com>
2014-10-14 17:14:56 -0400

    Added Stadiums table Added Stadiums CSV Seeded Database Added Stadiums model Added stadiums index and show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+filename = Rails.root.join("db/data/stadiums.csv")
+
+CSV.foreach(filename, headers: true) do |row|
+  row = row.to_hash
+  stadium = Stadium.find_or_create_by(row)
+  puts "#{stadium.name} in database."
+end
+
